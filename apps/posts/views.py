878a038057f3b63ebda1c5815af2065d29da8ec0
hengage <tunecarterkillz@gmail.com>
2022-11-25 23:14:22 +0100

    Feature: home page view

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from .models import Post
+class HomeView(generic.ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'post_list'
+    paginate_by = 6
