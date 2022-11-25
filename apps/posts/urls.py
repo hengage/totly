7c7b54b836e1645d67624dd722cba32e610b0d8a
hengage <tunecarterkillz@gmail.com>
2022-11-26 00:13:15 +0100

    feature: create post views and url

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-   path('', views.HomeView.as_view(), name='home')
+   path('', views.HomeView.as_view(), name='home'),
+   path('posts/create', views.CreatePostView.as_view(), name='create_post'),
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,7 +1,19 @@
 from django.views import generic
+from django.views.generic import edit
+
 from .models import Post
+from .forms import CreatePostForm
 class HomeView(generic.ListView):
     model = Post
     template_name = 'posts/home.html'
     context_object_name = 'post_list'
     paginate_by = 6
+
+class CreatePostView(edit.CreateView):
+    model = Post
+    form_class = CreatePostForm
+    template_name = 'posts/create_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
