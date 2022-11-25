3596dd7e2a26f159421e5d690865c288a566a908
hengage <tunecarterkillz@gmail.com>
2022-11-25 04:40:10 +0100

    create users app

--- totly/settings.py
+++ totly/settings.py
@@ -24,6 +24,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
