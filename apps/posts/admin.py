5047e1b9eff19ddc89f62219e71625ba69bd99c8
hengage <tunecarterkillz@gmail.com>
2022-11-25 23:11:05 +0100

    Feature: register Posts, category and comments in posts admin

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Post,  Comment, Category
 
-# Register your models here.
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra= 0
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_created']
+    inlines = [ CommentInline ]
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 0
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category_name', 'id', 'slug']
+    inlines = [  PostInline ]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'commentator']
+
+
+
+# admin.site.register(Post, PostAdmin) 
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Comment)
