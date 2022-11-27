9b33ace2b3271fe5ef9b5bc66f0e6d2169108cc6
hengage <tunecarterkillz@gmail.com>
2022-11-27 01:50:41 +0100

    Feature: install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './templates/*.html',
+    'apps/*/templates/*/*.html',
+    'apps/*/templates/*.html',
+
+    // Flowbite
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [ require('flowbite/plugin') ],
+}
