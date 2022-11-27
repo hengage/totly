08b0975a0adcb483dd616acee15186da35a97129
hengage <tunecarterkillz@gmail.com>
2022-11-27 02:49:38 +0100

    custom tailwind  colors and screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#0b1b4b',
+        'primary-color2':'#FB9129',
+        'button-hover':'#0e0761',
+        'sec-color': 'e78e26',
+        'pry-font-color': '#948894',
+        'heading-color': '#6b6a94'
+      },
+      screens: {
+        'xsm': '520px'
+      }
+    },
   },
   plugins: [ require('flowbite/plugin') ],
 }
