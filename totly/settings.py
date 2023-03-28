b69463a1802ea9cdf9d6a71a30537a56bdb32e68
hengage <tunecarterkillz@gmail.com>
2023-03-28 10:59:00 +0100

    Chore: add image to not found page

--- totly/settings.py
+++ totly/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
