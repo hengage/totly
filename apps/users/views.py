df7a15415ec1fbc84384463b344814b514c496c9
hengage <tunecarterkillz@gmail.com>
2023-06-16 15:42:31 +0100

    Feature: display primary email address and other email addresses on user profile page

--- apps/users/views.py
+++ apps/users/views.py
@@ -44,6 +44,8 @@ class UpdateUserView(
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        email_addresses = EmailAddress.objects.filter(user=self.request.user)
-        context['email_addresses'] = email_addresses
+        primary_email = EmailAddress.objects.filter(user=self.request.user).filter(primary=True).first()
+        other_email_addresses = EmailAddress.objects.filter(user=self.request.user).exclude(primary=True)
+        context['other_email_addresses'] = other_email_addresses
+        context['primary_email'] = primary_email
         return context
